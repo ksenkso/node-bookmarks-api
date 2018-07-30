0f5b397cf035b308a2e5bcfeed66780835287f34
ksenkso <yazunya@gmail.com>
2018-07-30 20:29:33 +0300

    Cleanup

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,7 +1,6 @@
 
 
 const Dashboard = function(req, res){
-	let user = req.user.id;
 	return res.json({success:true, message:'it worked', data:'user name is :'});
 };
 module.exports.Dashboard = Dashboard;
